5d2ef58003252c84398e4ebfff36252b91a05e38
kbrogie99 <kbrogie99@users.noreply.github.com>
2014-11-13 16:11:39 -0800

    Update addnumbers.cpp

--- addnumbers.cpp
+++ addnumbers.cpp
@@ -4,9 +4,9 @@ int main()
 	int x, y, total;
 	
 	std::cout << "What do you want to add?" << std::endl;
-    std::cin >> x >> y;
-    total = x + y;
-    std::cout << "This equals" << total << std::endl;
-
-    return 0;
+	std::cin >> x >> y;
+	total = x + y;
+	std::cout << "This equals" << total << std::endl;
+	
+	return 0;
 }
